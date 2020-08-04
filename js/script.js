6ae894bb920ef89ff9d67ca17050cdebb498a411
Jared Hood <jwh6ry@virginia.edu>
2020-08-04 17:15:25 -0400

    huge style update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Js for nav scroll slowly
+
+let mobileMenuBtn = document.querySelector('.js-open-mobile-menu');
+let mobileMenuEl = document.querySelector('.js-mobile-menu');
+
+
+mobileMenuBtn.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenuEl.classList.toggle('is-open');
+}
