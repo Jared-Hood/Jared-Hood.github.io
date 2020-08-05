c7f4ee2404142e330c7c452b7326b42ab5379b2d
Jared Hood <jwh6ry@virginia.edu>
2020-08-04 22:47:34 -0400

    fix id names

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// Js for nav scroll slowly
-
-// let mobileMenuBtn = document.querySelector('.js-open-mobile-menu');
-// let mobileMenuEl = document.querySelector('.js-mobile-menu');
-
-
-// mobileMenuBtn.addEventListener('click', toggleMobileMenu);
-
-// function toggleMobileMenu() {
-//     mobileMenuEl.classList.toggle('is-open');
-// }
-
 let aboutBtn = document.querySelector('.js-about-btn');
 let projectsBtn = document.querySelector('.js-projects-btn');
 let resumeBtn = document.querySelector('.js-resume-btn');
@@ -17,14 +5,15 @@ let contactBtn = document.querySelector('.js-contact-btn');
 let headerHeight = document.querySelector('.Header').style.height;
 let scrollTopIcon = document.querySelector('.js-scroll-top');
 
-aboutBtn.addEventListener('click', function() {scrollSection('#about');});
-projectsBtn.addEventListener('click', function() {scrollSection('#projects');});
-resumeBtn.addEventListener('click', function() {scrollSection('#resume');});
-contactBtn.addEventListener('click', function() {scrollSection('#contact');});
+aboutBtn.addEventListener('click', function() {scrollSection('#About');});
+projectsBtn.addEventListener('click', function() {scrollSection('#Projects');});
+resumeBtn.addEventListener('click', function() {scrollSection('#Resume');});
+contactBtn.addEventListener('click', function() {scrollSection('#Contact');});
 scrollTopIcon.addEventListener('click', scrollPageTop);
 
 function scrollSection(id) {
     let scrollEl = document.querySelector(id);
+    console.log(id);
     scrollEl.scrollIntoView({
         behavior: "smooth",
         block: "start"
